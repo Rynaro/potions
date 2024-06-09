9a742c31baa69f3540f3bf3d4c5133ed5717e03a
Henrique Aparecido Lavezzo <5271543+Rynaro@users.noreply.github.com>
2024-06-09 20:10:45 -0300

    add rustc for arm compiling

--- packages/debian/ruby.sh
+++ packages/debian/ruby.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 prepare_package() {
-  sudo apt-get install -y autoconf patch build-essential \
-    rustc libssl-dev libyaml-dev libreadline6-dev zlib1g-dev \
-    libgmp-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev \
-    libdb-dev uuid-dev
+  sudo apt-get install -y git-core curl zlib1g-dev build-essential \
+    libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 \
+    libxml2-dev libxslt1-dev libcurl4-openssl-dev \
+    software-properties-common libffi-dev rustc
 }
 
 prepare_package
