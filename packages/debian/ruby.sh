914d2d7cb51874faf16d5982aa054be0e3e9f3f9
Henrique Aparecido Lavezzo <5271543+Rynaro@users.noreply.github.com>
2024-06-09 19:27:35 -0300

    always accept the install

--- packages/debian/ruby.sh
+++ packages/debian/ruby.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 prepare_package() {
-  sudo apt-get install autoconf patch build-essential \
+  sudo apt-get install -y autoconf patch build-essential \
     rustc libssl-dev libyaml-dev libreadline6-dev zlib1g-dev \
     libgmp-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev \
     libdb-dev uuid-dev
