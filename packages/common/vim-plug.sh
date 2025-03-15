7b8689613ecc52c893086793c5b66892b4457ad4
Henrique Aparecido Lavezzo <5271543+Rynaro@users.noreply.github.com>
2025-03-15 17:34:44 -0300

    make vim-plug installation smoother

--- packages/common/vim-plug.sh
+++ packages/common/vim-plug.sh
@@ -4,12 +4,15 @@ source "$(dirname "$0")/packages/accessories.sh"
 
 # Function to install Vim-Plug
 install_package() {
-  if [ -d "${XDG_DATA_HOME:-$USER_HOME_FOLDER/.local/share}/nvim/site/autoload" ]; then
+  local target_dir="${XDG_DATA_HOME:-$USER_HOME_FOLDER/.local/share}/nvim/site/autoload"
+  local plug_file="$target_dir/plug.vim"
+  
+  if [ -f "$plug_file" ]; then
     log "Vim-Plug is already installed."
   else
     log "Installing Vim-Plug..."
-    sh -c 'curl -fLo "${XDG_DATA_HOME:-$USER_HOME_FOLDER/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+    mkdir -p "$target_dir"
+    curl -fLo "$plug_file" https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
   fi
 }
 
